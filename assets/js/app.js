8eae5cad566c8cfd941e44e064275ee9fc73ff25
frankalvarezdev <frankcontacto64@gmail.com>
2021-11-08 13:06:39 -0500

    integracion de ECMAScript 6

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,8 @@
-const swup = new Swup();  
+import 'https://unpkg.com/swup@2.0.14/dist/swup.min.js';
+import { test } from './test.js';
+
+// swup init
+const swup = new Swup();
+
+// prueba
+console.log(test);
