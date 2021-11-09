1a6c3a03afe6bb020e2ccb34d50194a0995c0367
frankalvarezdev <frankcontacto64@gmail.com>
2021-11-09 08:00:38 -0500

    Lazy Images

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,13 @@
 import 'https://unpkg.com/swup@2.0.14/dist/swup.min.js';
 import { test } from './test.js';
+import LazyImages from './utils/lazyImages.js';
 
 // swup init
 const swup = new Swup();
+
+// lazy load de imagenes
+const lazyImages = new LazyImages();
+lazyImages.start();
 
 // prueba
 console.log(test);
