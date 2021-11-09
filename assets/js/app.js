44f9d71ef9e2acdb438c554a3cf7e4cbb3b5647a
frankalvarezdev <frankcontacto64@gmail.com>
2021-11-09 14:54:54 -0500

    add pregress bar plugin

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
 import 'https://unpkg.com/swup@2.0.14/dist/swup.min.js';
+import 'https://unpkg.com/@swup/progress-plugin@1.0.1/dist/SwupProgressPlugin.min.js';
 import 'https://unpkg.com/micromodal/dist/micromodal.min.js';
 import 'https://unpkg.com/@popperjs/core@2/dist/umd/popper.min.js';
 import 'https://unpkg.com/tippy.js@6/dist/tippy-bundle.umd.js';
@@ -22,7 +23,16 @@ tippy('#share', {
 });
 
 // swup init
-const swup = new Swup();
+const swup = new Swup({
+    cache: false,
+    plugins: [
+        new SwupProgressPlugin({
+            className: 'swup-progress-bar',
+            transition: 500,
+            delay: 0
+        })
+    ]
+});
 swup.on('contentReplaced', () => {
     // en cuanto el sitio tenga cambios carga las imagenes
     lazyImages.start();
