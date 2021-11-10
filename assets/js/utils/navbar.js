40d128d275747dfb721ce74a36fb7b526e5386de
frankalvarezdev <frankcontacto64@gmail.com>
2021-11-10 14:27:50 -0500

    Cambios de enlaces de activacion

--- assets/js/utils/navbar.js
+++ assets/js/utils/navbar.js
@@ -8,7 +8,7 @@ class Navbar {
             link.classList.remove('is-active');
 
             // si el path del enlace es igual al del que esta activo actualmente -> agrega la clase is-active
-            if (path === document.location.pathname) {
+            if (path == document.location.pathname) {
                 link.classList.add('is-active');
             }
         }
