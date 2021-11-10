6386d7e510eac7ae83c99aa2f2630da73150de40
frankalvarezdev <frankcontacto64@gmail.com>
2021-11-10 14:18:13 -0500

    Solucion de error de js

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,26 +4,10 @@ import 'https://unpkg.com/micromodal/dist/micromodal.min.js';
 import 'https://unpkg.com/@popperjs/core@2/dist/umd/popper.min.js';
 import 'https://unpkg.com/tippy.js@6/dist/tippy-bundle.umd.js';
 import 'https://unpkg.com/@swup/forms-plugin@1.1.1/dist/SwupFormsPlugin.min.js';
-import 'https://unpkg.com/autosize@5.0.1/dist/autosize.min.js';
 import LazyImages from './utils/lazyImages.js';
 import Navbar from './utils/navbar.js';
+import { router } from './routes.js';
 
-// lazy load de imagenes
-const lazyImages = new LazyImages();
-lazyImages.start();
-Navbar.checkActiveLinks();
-
-autosize(document.querySelector('textarea'));
-
-// tipy init
-tippy('#share', {
-    content: document.getElementById('template').innerHTML,
-    interactive: true,
-    placement: 'bottom',
-    trigger: 'click',
-    allowHTML: true,
-    theme: 'socialcube'
-});
 
 // swup init
 const swup = new Swup({
@@ -37,11 +21,27 @@ const swup = new Swup({
         new SwupFormsPlugin()
     ]
 });
+
+const lazyImages = new LazyImages();
+lazyImages.start(); // lazy load de imagenes
+Navbar.checkActiveLinks();
+router.run();
+
+MicroModal.init(); // modal init
+
 swup.on('contentReplaced', () => {
     // en cuanto el sitio tenga cambios carga las imagenes
     lazyImages.start();
     Navbar.checkActiveLinks();
+    router.run();
 });
 
-// modal init
-MicroModal.init();
+// tipy init
+// tippy('#share', {
+//     content: document.getElementById('template').innerHTML,
+//     interactive: true,
+//     placement: 'bottom',
+//     trigger: 'click',
+//     allowHTML: true,
+//     theme: 'socialcube'
+// });
