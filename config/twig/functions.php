ab3bda565d398284b20f0507428039ebb5ff91c0
frankalvarezdev <frankcontacto64@gmail.com>
2021-11-10 20:26:36 -0500

    se integro gravatar

--- config/twig/functions.php
+++ config/twig/functions.php
@@ -1,11 +1,21 @@
 <?php
 
-$input = new \Twig\TwigFunction('input', function($name) {
+$input = new \Twig\TwigFunction('input', function ($name) {
     return input($name);
 });
-$fromnow = new \Twig\TwigFunction('fromNow', function($date) {
+
+$fromnow = new \Twig\TwigFunction('fromNow', function ($date) {
     return fromNow($date);
+});
+
+$userAvatar = new \Twig\TwigFunction('userAvatar', function ($uuid) {
+    if ($uuid === 'default.jpg') {
+        return 'https://www.gravatar.com/avatar/' . md5(strtolower(trim($_SESSION['user']->email))) . '?d=mp';
+    } else {
+        return BASE_URL . 'images/' . $uuid;
+    }
 });
 
 $twig->addFunction($input);
 $twig->addFunction($fromnow);
+$twig->addFunction($userAvatar);
